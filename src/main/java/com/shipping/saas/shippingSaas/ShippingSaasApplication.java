1aa2b7770f6f99073f5c6f651b8a1fef70188d19
jevonne <jevonne63@gmail.com.com>
2025-05-14 05:03:12 -0500

    initial commit

---
+++ src/main/java/com/shipping/saas/shippingSaas/ShippingSaasApplication.java
@@ -0,0 +1,13 @@
+package com.shipping.saas.shippingSaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shipping/saas/shippingSaas/ShippingSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipping.saas.shippingSaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

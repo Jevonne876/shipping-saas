5603d83e95a7d7ee1be4113bff98ca9b9f6991db
jevonne <jevonne63@gmail.com.com>
2025-05-17 21:14:48 -0500

    need to start working on authentication for ClientUser and Customers

--- src/main/java/com/shipping/saas/shippingSaas/domain/ClientUser.java
+++ src/main/java/com/shipping/saas/shippingSaas/domain/ClientUser.java
@@ -34,9 +34,9 @@ public class ClientUser {
     @Column(nullable = false)
     private String passwordHash;
 
-    @ManyToOne
-    @JoinColumn(name = "role_id")
-    private Role roleId;
+//    @ManyToOne
+//    @JoinColumn(name = "role_id")
+//    private Role roleId;
 
     @Builder.Default
     private boolean isActive = true;
